b3a798b60d09f35e54ac6649247b9b18b5828abf
Lolo <lolognz@gmail.com>
2019-08-15 13:31:56 +0200

    Updates

---
+++ precache-manifest.030d03837571ff78169fe3ebff229351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4874a33f4f3865d606a8cafff60d18",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "a4eb836eea5626d07d16",
+    "url": "/web/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "871d9e766b1715123198",
+    "url": "/web/static/js/2.844055f1.chunk.js"
+  },
+  {
+    "revision": "a4eb836eea5626d07d16",
+    "url": "/web/static/js/main.28167d50.chunk.js"
+  },
+  {
+    "revision": "70c22b3514e757e782ce",
+    "url": "/web/static/js/runtime~main.47739002.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  }
+]);
